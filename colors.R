6e45971681747d2daddd288029b33ba862693fad
Aaron Berdanier <aaron.berdanier@gmail.com>
2016-02-05 15:35:11 -0500

    fixed number of values

--- colors.R
+++ colors.R
@@ -1,20 +1,18 @@
-quantColor <- function(x,col=c("#ca0020","#f7f7f7","#0571b0"),n=length(x)){
+quantColor <- function(x,col=c("#ca0020","#f7f7f7","#0571b0")){
   # x is a vector of values
   # col is a sequence of colors, optional
-  # n is a number of distinct colors, optional
   Pal <- colorRampPalette(col)
-  brk <- quantile(x, probs=seq(0,1,length.out=n))
-  Pal(n)[cut(x, breaks=brk, include.lowest=T)]
+  brk <- quantile(x, probs=seq(0,1,length.out=length(x)))
+  Pal(length(x))[cut(x, breaks=brk, include.lowest=T)]
 }
 
-quantColorLegend <- function(x,col=c("#ca0020","#f7f7f7","#0571b0"),n=length(x)){
+quantColorLegend <- function(x,col=c("#ca0020","#f7f7f7","#0571b0")){
   # x is a vector of values
   # col is a sequence of colors, optional
-  # n is a number of distinct colors, optional
   Pal <- colorRampPalette(col)
-  brk <- quantile(x, probs=seq(0,1,length.out=n))
-  colors <- Pal(n)[cut(x, breaks=brk, include.lowest=T)]
-  image(1, brk, t(matrix(1:n)), col=Pal(n), xaxt="n", xlab="")
+  brk <- quantile(x, probs=seq(0,1,length.out=length(x)))
+  colors <- Pal(length(x))[cut(x, breaks=brk, include.lowest=T)]
+  image(1, brk, t(matrix(1:length(x))), col=Pal(length(x)), xaxt="n", xlab="")
 }
 
 randval <- exp(rnorm(50,0,1.2))
